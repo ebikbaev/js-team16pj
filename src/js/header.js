47b83768a7c440bbd3d939a49f579a4b4eed03b2
anisteisha <lereenaarakurt@gmail.com>
2024-07-08 20:10:53 +0100

    proces

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Elements
+  
   const menuBtn = document.querySelector('.header-menu-btn');
+      
   const desktopMenu = document.querySelector('.header-list-navigation');
+      
   const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+      
   const mobileMenu = document.getElementById('mobile-menu');
+      
   const mobileMenuClose = document.getElementById('menu-close');
+      
   const mobileMenuLinks = document.querySelectorAll('.mobile-menu-list');
+      
   const orderBtns = document.querySelectorAll('.header-link-btn, .mobile-menu-order-btn');
+      
 
-  // Helper function for smooth scrolling
+  
   function smoothScroll(target) {
     const targetElement = document.querySelector(target);
     if (targetElement) {
@@ -20,14 +27,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // Desktop Menu Toggle
+  
   if (menuBtn) {
     menuBtn.addEventListener('click', () => {
       desktopMenu.classList.toggle('is-hidden');
     });
   }
 
-  // Mobile Menu Open
+  
   if (mobileMenuBtn) {
     mobileMenuBtn.addEventListener('click', () => {
       mobileMenu.classList.add('open');
@@ -35,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // Mobile Menu Close
+  
   if (mobileMenuClose) {
     mobileMenuClose.addEventListener('click', () => {
       mobileMenu.classList.remove('open');
@@ -43,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // Mobile Menu Link Click
+  
   if (mobileMenuLinks) {
     mobileMenuLinks.forEach(link => {
       link.addEventListener('click', (event) => {
@@ -56,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // Order Button Click
+  
   if (orderBtns) {
     orderBtns.forEach(btn => {
       btn.addEventListener('click', (event) => {
