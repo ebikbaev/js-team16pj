b856ed92dd1d48a1e9920f739acd47adc247810f
Serhii Ivashchenko <s.ivashchenko.1989@gmail.com>
2024-07-09 20:49:10 +0300

    fixed accordion js and css

--- src/js/about-me-ac.js
+++ src/js/about-me-ac.js
@@ -1,7 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const accordion = new Accordion('.accordion-container', {
-    duration: 250,
+const accordion = new Accordion('.about-accordion-container', {
+    duration: 500,
     openOnInit: [0],
 });
