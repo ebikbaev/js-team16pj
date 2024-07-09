0819ca5325ab66e92dbe936db665bd424ae5e2e6
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-07-09 05:45:47 +0300

    correction

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,5 @@
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
 
-// new Accordion('.accordion-container');
+new Accordion('.accordion-container');
