61396f190c81e4e0086f23824d51e6221be6a2da
Serhii Borysenko <borisenko37@gmail.com>
2024-07-09 11:31:14 +0200

    add icons

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,8 +1,8 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-const nextButton = document.querySelector('.button-next');
-const prevButton = document.querySelector('.button-prev');
+const nextButton = document.querySelector('.button-right');
+const prevButton = document.querySelector('.button-left');
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
@@ -14,4 +14,32 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
+  on: {
+    init: function () {
+      classSwitcher(this);
+    },
+    slideChange: function () {
+      classSwitcher(this);
+    },
+    reachEnd: function () {
+      classSwitcher(this);
+    },
+    reachBeginning: function () {
+      classSwitcher(this);
+    },
+  },
 });
+
+function classSwitcher(swiper) {
+  if (swiper.isBeginning) {
+    prevButton.classList.add('btn-off');
+  } else {
+    prevButton.classList.remove('btn-off');
+  }
+
+  if (swiper.isEnd) {
+    nextButton.classList.add('btn-off');
+  } else {
+    nextButton.classList.remove('btn-off');
+  }
+}
